e3c1c27c280b4da8ebbbf2af1ab398a85be58627
andermendz <andersonmendz19@gmail.com>
2023-01-26 18:50:23 -0500

    first get request

--- forms/forms.php
+++ forms/forms.php
@@ -1,9 +1,13 @@
 <p> Testing submit</p>
 <form >
-<input type="text" name = "subguest" id="guest">
+<input type="text" name = "submt" id="guest">
 <input type="submit">
 </form>
+
+
 <?php
+
+// get request via form
 print_r($_GET);
 
 ?>
