50cd43726ec1ebc2a5c4c26abc87eafb25cbb56e
andermendz <andersonmendz19@gmail.com>
2023-01-26 20:27:27 -0500

    data validation

---
+++ forms/dvalidation.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>guess</title>
+</head>
+<body>
+    <h1>Welcome Bud</h1>
+    <p>
+
+    <?php
+    if ( ! isset($_GET["person"]) ){
+        echo "dont exist";
+    } else if ( strlen($_GET["person"] < 1) ){
+        echo "too short";
+    } else if ( is_numeric($_GET["person"]) ){
+        echo "guess is not a number";
+    } else if ( $_GET["person"] == "ander"){
+        echo "congratulations";
+    } else {
+        echo "nanais";
+    }
+
+
+        ?>
+    </p>
+
+</body>
+
+</html>
