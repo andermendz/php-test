85ed84ac61200955f9b01bc702e8795800aaf718
andermendz <andersonmendz19@gmail.com>
2023-01-28 22:01:01 -0500

    database connection and table

--- PDO/pdo.php
+++ PDO/pdo.php
@@ -4,12 +4,26 @@ $dbc = new PDO('mysql:host=localhost;port=3306;dbname=misc','ander','zap');
 
 $users = $dbc->query("SELECT * FROM users");
 
+echo "<TABLE border=1";
+echo "<tr><th>";
+echo "Name";
+echo "</th><th>";
+echo "Email";
+echo "</th><th>";
+echo "Password";
+echo "</th></tr>";
 while ($row = $users ->fetch(PDO::FETCH_ASSOC)){
-    print_r($row);
-    echo "<br>";
+
+    echo "<tr><td>";
+    echo $row['name'];
+    echo "</td><td>";
+    echo $row['email'];
+    echo "</td><td>";
+    echo $row['password'];
+    echo "</td></tr>";
 }
 
 
-
+echo "</table>";
 
 ?>
