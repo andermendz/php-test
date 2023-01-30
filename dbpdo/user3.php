a5eccd159dd8f275a5320599db9743e5a841b282
andermendz <andersonmendz19@gmail.com>
2023-01-30 18:22:16 -0500

    done

--- dbpdo/user3.php
+++ dbpdo/user3.php
@@ -12,6 +12,7 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])
             ':name' => $_POST['name'],
             ':email' => $_POST['email'],
             ':password' => $_POST['password']
+            
         )
     );
 }
